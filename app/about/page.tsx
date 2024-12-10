a07c245284f5cf57a36f69bdc3e10bfdb5e13bbb
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-12-10 14:47:24 -0600

    small update to the about pages.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from 'next'
-import { Navbar } from '@/components/navbar'
-import { Footer } from '@/components/footer'
+
 import AboutPage from '@/components/About Page/about-us'
+import { Footer } from '@/components/footer'
+import { Navbar } from '@/components/navbar'
 
 export const metadata: Metadata = {
   title: 'Apply to Join Our Team | Outkastig',
@@ -11,14 +12,14 @@ export const metadata: Metadata = {
 export default function AboutUs() {
   return (
     <>
-      <Navbar />
+     <Navbar />
       <main className="flex-1">
         <section className="container mx-auto px-4 py-16">
     <AboutPage />
        
         </section>
       </main>
-      <Footer />
+     <Footer />
     </>
   )
 }
